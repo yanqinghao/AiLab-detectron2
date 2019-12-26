053bef76c61822f8be8b4bacb1b169bbaaaa0217
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-26 16:26:29 +0800

    stream predict

---
+++ components/stream/SPPredictor.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.app.arguments import Folder, Json, Model, Int, Float
+from suanpan.app import app
+from suanpan.log import logger
+from arguments import CFGModel
+from utils.tools import find_files
+
+
+@app.input(Model(key="inputModel", type=CFGModel))
+@app.input(Folder(key="inputData"))
+@app.param(Int(key="__gpu", default=0))
+@app.param(Float(key="ScoreThreshTest", default=0.5))
+@app.output(Json(key="outputData1"))
+@app.output(Folder(key="outputData2"))
+def SPPredictor(context):
+    args = context.args
+    model = args.inputModel
+    device = "cuda" if args.__gpu else "cpu"
+    logger.info("**********Use {} Device for Predicting**********".format(device))
+    model.device(device)
+    model.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.ScoreThreshTest
+
+    images = find_files(args.inputData)
+
+    outputs = model.predict(images)
+    pred_data = []
+    for output in outputs:
+        pred_data.append(
+            {
+                "image_size": output["instances"].image_size,
+                "pred_boxes": output["instances"].pred_boxes.tensor.tolist(),
+                "pred_classes": output["instances"].pred_classes.tolist(),
+                "pred_masks": output["instances"].pred_masks.tolist(),
+                "scores": output["instances"].scores.tolist(),
+            }
+        )
+    return pred_data
+
+
+if __name__ == "__main__":
+    SPPredictor()
