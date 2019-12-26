9207b4302feb08494eed0d2d9eaf0fabe04e1a32
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-26 19:34:42 +0800

    file type

--- utils/tools.py
+++ utils/tools.py
@@ -1,14 +1,14 @@
 import os
 
 
-def find_files(folder):
+def find_files(folder, file_type):
     files_ = []
     list = [i for i in os.listdir(folder)]
     for i in range(0, len(list)):
         path = os.path.join(folder, list[i])
         if os.path.isdir(path):
-            files_.extend(find_files(path))
+            files_.extend(find_files(path, file_type))
         if not os.path.isdir(path):
-            if path.lower().endswith("pdf"):
+            if path.lower().endswith(file_type):
                 files_.append(path)
     return files_
