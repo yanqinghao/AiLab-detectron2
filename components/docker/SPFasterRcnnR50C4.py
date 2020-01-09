14564e76e14791e1a544a1de9baa6aceb5d0031d
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-09 11:23:08 +0800

    faster_rcnn_R_50_C4 model

---
+++ components/docker/SPFasterRcnnR50C4.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.app import app
+from suanpan.app.arguments import Model, Bool
+from arguments import CFGModel
+
+
+@app.param(Bool(key="usePredWeight", default=True))
+@app.output(Model(key="model", type=CFGModel))
+def SPFasterRcnnR50C4(context):
+    args = context.args
+    if args.usePredWeight:
+        args.model.load_model(
+            "./detectron2/configs/COCO-Detection/faster_rcnn_R_50_C4_3x.yaml",
+            "common/model/detectron2/ImageNetPretrained/MSRA/model_final_f97cb7.pkl",
+        )
+    else:
+        args.model.load_model(
+            "./detectron2/configs/COCO-Detection/faster_rcnn_R_50_C4_3x.yaml",
+            "",
+        )
+    return args.model
+
+
+if __name__ == "__main__":
+    SPFasterRcnnR50C4()
