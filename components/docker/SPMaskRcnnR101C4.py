57b1992d674deeedad4b77b16abf9e73c7c66593
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-09 17:29:43 +0800

    mask_rcnn_R_101_C4 model

---
+++ components/docker/SPMaskRcnnR101C4.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.app import app
+from suanpan.app.arguments import Model, Bool
+from arguments import CFGModel
+
+
+@app.param(Bool(key="usePredWeight", default=True))
+@app.output(Model(key="model", type=CFGModel))
+def SPMaskRcnnR101C4(context):
+    args = context.args
+    if args.usePredWeight:
+        args.model.load_model(
+            "./detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml",
+            "common/model/detectron2/ImageNetPretrained/MSRA/model_final_a2914c.pkl",
+        )
+    else:
+        args.model.load_model(
+            "./detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml",
+            "",
+        )
+    return args.model
+
+
+if __name__ == "__main__":
+    SPMaskRcnnR101C4()
