05d6016ae3f8c397d08eba485b97fd2a25848f3c
YanQinghao <woshiyanqinghao@gmail.com>
2020-04-09 10:25:24 +0800

    vpc

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,9 @@
 NAMESPACE=("shuzhi-amd64")
 for i in ${NAMESPACE[*]}
 do
-    docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1 -f docker/docker_detectron2_gpu/Dockerfile .
-    docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1 -f docker/docker_detectron2_gpu/Dockerfile .
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1
 
-    docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1 -f docker/docker_detectron2/Dockerfile .
-    docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1 -f docker/docker_detectron2/Dockerfile .
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1
 done
