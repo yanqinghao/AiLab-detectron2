4bf40d27ff00b900fad762142383daf11598df3c
YanQinghao <woshiyanqinghao@gmail.com>
2020-04-09 10:23:49 +0800

    fix

--- tools/build.sh
+++ tools/build.sh
@@ -3,12 +3,7 @@ for i in ${NAMESPACE[*]}
 do
     docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1 -f docker/docker_detectron2_gpu/Dockerfile .
     docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker-gpu:$1
-    docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-stream-gpu:$1 -f docker/stream_detectron2_gpu/Dockerfile .
-    docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-stream-gpu:$1
-    sed -i "s/cpu/cu100/g" .dockerignore
+
     docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1 -f docker/docker_detectron2/Dockerfile .
     docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-docker:$1
-    docker build --build-arg NAME_SPACE=${i} -t registry.cn-shanghai.aliyuncs.com/${i}/detectron2-stream:$1 -f docker/stream_detectron2/Dockerfile .
-    docker push registry.cn-shanghai.aliyuncs.com/${i}/detectron2-stream:$1
-    sed -i "s/cu100/cpu/g" .dockerignore
 done
